1dbae557e0f5ba8f1a69dcbf4d6a81d95a05db1b
Juho Kangas <juho.kangas@aalto.fi>
2024-01-16 13:44:44 +0200

    1.7 done

--- Osa1/unicafe/src/App.jsx
+++ Osa1/unicafe/src/App.jsx
@@ -1,6 +1,5 @@
 import { useState } from 'react'
 
-
 const Button = ({ handleClick, text }) => (
   <button onClick={handleClick}>
     {text}
@@ -14,20 +13,27 @@ const App = () => {
   const [good, setGood] = useState(0)
   const [neutral, setNeutral] = useState(0)
   const [bad, setBad] = useState(0)
+  const [average, setAverage] = useState(0)
+  const [total, setTotal] = useState(0)
 
   const handleGood = () => {
     setGood(good + 1)
     console.log('pressed good')
+    setAverage(average + 1)
+    setTotal(total + 1)
   }
 
   const handleNeutral = () => {
     setNeutral(neutral + 1)
     console.log('pressed neutral')
+    setTotal(total + 1)
   }
 
   const handleBad = () => {
     setBad(bad + 1)
     console.log('pressed bad')
+    setAverage(average - 1)
+    setTotal(total + 1)
   }
 
   return (
@@ -40,6 +46,9 @@ const App = () => {
       <p>good {good}</p>
       <p>neutral {neutral}</p>
       <p>bad {bad}</p>
+      <p>all {total}</p>
+      <p>average {average/total}</p>
+      <p>positive {good/total*100}%</p>
     </div>
   )
 }
