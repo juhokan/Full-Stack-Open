56007498ea1ff9f9eaa59211aa237e3579abbd4d
Juho Kangas <juho.kangas@aalto.fi>
2024-06-11 17:03:15 +0300

    tests for 4.8 & 4.9 created

---
+++ Osa4/blogilista-backend/tests/blog_api.test.js
@@ -0,0 +1,71 @@
+const { test, after, beforeEach } = require('node:test')
+const mongoose = require('mongoose')
+const assert = require('node:assert')
+const supertest = require('supertest')
+const app = require('../app')
+const Blog = require('../models/blog')
+
+const api = supertest(app)
+
+const initialBlogs = [
+  {
+    _id: "5a422a851b54a676234d17f7",
+    title: "React patterns",
+    author: "Michael Chan",
+    url: "https://reactpatterns.com/",
+    likes: 7,
+    __v: 0
+  },
+  {
+    _id: "5a422aa71b54a676234d17f8",
+    title: "Go To Statement Considered Harmful",
+    author: "Edsger W. Dijkstra",
+    url: "http://www.u.arizona.edu/~rubinson/copyright_violations/Go_To_Considered_Harmful.html",
+    likes: 5,
+    __v: 0
+  },
+  {
+    _id: "5a422b3a1b54a676234d17f9",
+    title: "Canonical string reduction",
+    author: "Edsger W. Dijkstra",
+    url: "http://www.cs.utexas.edu/~EWD/transcriptions/EWD08xx/EWD808.html",
+    likes: 12,
+    __v: 0
+  }
+]
+
+beforeEach(async () => {
+  await Blog.deleteMany({})
+  let blogObject = new Blog(initialBlogs[0])
+  await blogObject.save()
+  blogObject = new Blog(initialBlogs[1])
+  await blogObject.save()
+  blogObject = new Blog(initialBlogs[2])
+  await blogObject.save()
+})
+
+test('blogs are returned as json', async () => {
+  await api
+    .get('/api/blogs')
+    .expect(200)
+    .expect('Content-Type', /application\/json/)
+})
+
+test('there are three blogs', async () => {
+  const response = await api.get('/api/blogs')
+
+  assert.strictEqual(response.body.length, initialBlogs.length)
+})
+
+test('identifying field is id', async () => {
+  const response = await api.get('/api/blogs');
+  
+  response.body.forEach(blog => {
+    assert.strictEqual(typeof blog.id, 'string');
+    assert.notStrictEqual(blog._id, blog.id); // Ensure _id is not present in the response
+  });
+});
+
+after(async () => {
+  await mongoose.connection.close()
+})
