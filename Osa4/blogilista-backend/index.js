4869e329393c18f2f0b53caff6a9b26672cb3172
Juho Kangas <juho.kangas@aalto.fi>
2024-06-11 12:42:24 +0300

    modules created

--- Osa4/blogilista-backend/index.js
+++ Osa4/blogilista-backend/index.js
@@ -1,53 +1,6 @@
-const express = require('express');
-const app = express();
-const cors = require('cors');
-const mongoose = require('mongoose');
-require('dotenv').config()
+const app = require('./app') // varsinainen Express-sovellus
 const config = require('./utils/config')
 const logger = require('./utils/logger')
-
-const blogSchema = mongoose.Schema({
-  title: String,
-  author: String,
-  url: String,
-  likes: Number
-});
-
-
-const Blog = mongoose.model('Blog', blogSchema);
-
-app.use(express.static('dist'));
-app.use(express.json());
-app.use(cors());
-
-mongoose.set('strictQuery', false);
-
-
-mongoose.connect(config.MONGODB_URI)
-  .then(() => {
-    console.log('Connected to MongoDB');
-  })
-  .catch((error) => {
-    console.error('Error connecting to MongoDB:', error.message);
-  });
-
-app.get('/api/blogs', (request, response) => {
-  Blog
-    .find({})
-    .then(blogs => {
-      response.json(blogs);
-    });
-});
-
-app.post('/api/blogs', (request, response) => {
-  const blog = new Blog(request.body);
-
-  blog
-    .save()
-    .then(result => {
-      response.status(201).json(result);
-    });
-});
 
 app.listen(config.PORT, () => {
   logger.info(`Server running on port ${config.PORT}`)
