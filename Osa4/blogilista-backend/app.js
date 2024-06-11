284c68f06c4d505ba4c2d6171368bffa5ac6bca1
Juho Kangas <juho.kangas@aalto.fi>
2024-06-11 12:47:16 +0300

    refractoring, 4.1 and 4.2 complete

--- Osa4/blogilista-backend/app.js
+++ Osa4/blogilista-backend/app.js
@@ -9,7 +9,9 @@ const mongoose = require('mongoose')
 
 mongoose.set('strictQuery', false)
 
+
 logger.info('connecting to', config.MONGODB_URI)
+
 
 mongoose.connect(config.MONGODB_URI)
   .then(() => {
@@ -19,14 +21,14 @@ mongoose.connect(config.MONGODB_URI)
     logger.error('error connection to MongoDB:', error.message)
   })
 
+
 app.use(cors())
 app.use(express.static('dist'))
 app.use(express.json())
 app.use(middleware.requestLogger)
-
 app.use('/api/blogs', blogsRouter)
-
 app.use(middleware.unknownEndpoint)
 app.use(middleware.errorHandler)
+
 
 module.exports = app
