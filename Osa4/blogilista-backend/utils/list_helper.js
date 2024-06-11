8ec3b3f85f90d7f446885b2d74de594dece82744
Juho Kangas <juho.kangas@aalto.fi>
2024-06-11 13:33:07 +0300

    tests for 4.5 created

--- Osa4/blogilista-backend/utils/list_helper.js
+++ Osa4/blogilista-backend/utils/list_helper.js
@@ -6,7 +6,12 @@ const totalLikes = (blogs) => {
   return blogs.reduce((total, blog) => total + blog.likes, 0)
 }
 
+const favoriteBlog = (blogs) => {
+  return blogs.reduce((blogA, blogB) => (blogA.likes > blogB.likes ? blogA : blogB))
+}
+
 module.exports = {
   dummy,
-  totalLikes
+  totalLikes,
+  favoriteBlog
 }
