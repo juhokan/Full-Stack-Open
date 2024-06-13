e3f0215ad5c3cfbe6190686a48d19f4f6cfd8441
Juho Kangas <juho.kangas@aalto.fi>
2024-06-13 12:46:29 +0300

    create 5.6 & 5.7

--- Osa5/bloglist-frontend/src/components/Blog.jsx
+++ Osa5/bloglist-frontend/src/components/Blog.jsx
@@ -1,7 +1,41 @@
-const Blog = ({ blog }) => (
-  <div>
-    {blog.title} {blog.author}
-  </div>  
-)
+import React, { useState } from "react";
 
-export default Blog
+const Blog = ({ blog }) => {
+  const [isVisible, setIsVisible] = useState(false);
+
+  const toggleVisibility = () => {
+    setIsVisible(!isVisible);
+  };
+
+  const HiddenBlog = () => {
+    return (
+      <div>
+        {blog.title} {blog.author}
+        <button onClick={toggleVisibility}>show</button>
+      </div>
+    );
+  };
+
+  const VisibleBlog = () => {
+    return (
+      <div>
+        <div>{blog.title}</div>
+        <div>{blog.author}</div>
+        <div>{blog.url}</div>
+        <div>
+          Likes: {blog.likes}
+          <button>Like</button>
+        </div>
+        <button onClick={toggleVisibility}>hide</button>
+      </div>
+    );
+  };
+
+  return (
+    <div className="blog">
+      {isVisible ? <VisibleBlog /> : <HiddenBlog />}
+    </div>
+  );
+};
+
+export default Blog;
