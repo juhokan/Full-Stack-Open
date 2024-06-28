ad6caf232743149b487c549d72ec2f09e4dd7870
Juho Kangas <juho.kangas@aalto.fi>
2024-06-28 20:37:34 +0300

    get initial state from backend 6.14

--- Osa6/redux-anecdotes/src/main.jsx
+++ Osa6/redux-anecdotes/src/main.jsx
@@ -2,7 +2,14 @@ import ReactDOM from 'react-dom/client'
 import { Provider } from 'react-redux'
 import App from './App'
 import { store } from './store'
+import service from './services/anecdotes'
+import { appendAnecdote } from './reducers/anecdoteReducer'
 
+service.getAll().then(anecdotes => {
+  anecdotes.forEach(anecdote => {
+    store.dispatch(appendAnecdote(anecdote))
+  });
+})
 
 ReactDOM.createRoot(document.getElementById('root')).render(
   <Provider store={store}>
