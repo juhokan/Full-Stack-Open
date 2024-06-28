3dee0ea9a2728ff205c272cf4324da82a4897d06
Juho Kangas <juho.kangas@aalto.fi>
2024-06-28 22:46:59 +0300

    create react query app 6.20 - 6.22

---
+++ Osa6/query-anecdotes/src/components/AnecdoteForm.jsx
@@ -0,0 +1,36 @@
+import { useMutation, useQueryClient } from '@tanstack/react-query'
+import { createAnecdote } from '../requests'
+
+const AnecdoteForm = () => {
+  const queryClient = useQueryClient()
+
+  const { mutate } = useMutation({
+    mutationFn: createAnecdote,
+    onSuccess: (newAnecdote) => {
+      const anecdotes = queryClient.getQueryData('anecdotes')
+      queryClient.setQueryData('anecdotes', anecdotes.concat(newAnecdote))
+    },
+  })
+
+  const onCreate = (event) => {
+    event.preventDefault()
+    const content = event.target.anecdote.value.trim()
+    if (content) {
+      const newAnecdote = { content, votes: 0 }
+      event.target.anecdote.value = ''
+      mutate(newAnecdote)
+    }
+  }
+
+  return (
+    <div>
+      <h3>create new</h3>
+      <form onSubmit={onCreate}>
+        <input name='anecdote' />
+        <button type="submit">create</button>
+      </form>
+    </div>
+  )
+}
+
+export default AnecdoteForm
