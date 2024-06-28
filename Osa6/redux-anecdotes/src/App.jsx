d87c322ff286573c63a0df3b31537ee062a87650
Juho Kangas <juho.kangas@aalto.fi>
2024-06-28 17:47:31 +0300

    refactor anecdote list 6.8

--- Osa6/redux-anecdotes/src/App.jsx
+++ Osa6/redux-anecdotes/src/App.jsx
@@ -1,40 +1,13 @@
-import { useSelector, useDispatch } from 'react-redux'
 import AnecdoteForm from './components/AnecdoteForm'
+import AnecdoteList from './components/AnecdoteList'
 
 const App = () => {
-  const anecdotes = useSelector(state => state)
-  const dispatch = useDispatch()
-
-  const vote = (anecdote) => {
-    const votes = anecdote.votes + 1
-    dispatch({
-      type: 'VOTE',
-      payload: {
-        content: anecdote.content,
-        votes: votes,
-        id: anecdote.id,
-      }
-    })
-  }
-
-  const sortedAnecdotes = anecdotes.slice().sort((a, b) => b.votes - a.votes)
 
   return (
-    <div>
-      <h2>Anecdotes</h2>
-      {sortedAnecdotes.map(anecdote =>
-        <div key={anecdote.id}>
-          <div>
-            {anecdote.content}
-          </div>
-          <div>
-            has {anecdote.votes}
-            <button onClick={() => vote(anecdote)}>vote</button>
-          </div>
-        </div>
-      )}
+    <>
+      <AnecdoteList />
       <AnecdoteForm />
-    </div>
+    </>
   )
 }
 
---
+++ Osa6/redux-anecdotes/src/components/AnecdoteList.jsx
@@ -0,0 +1,40 @@
+import { useSelector, useDispatch } from "react-redux"
+
+const AnecdoteList = () => {
+  const anecdotes = useSelector(state => state)
+  const dispatch = useDispatch()
+
+  const vote = (anecdote) => {
+
+    const votes = anecdote.votes + 1
+    dispatch({
+      type: 'VOTE',
+      payload: {
+        content: anecdote.content,
+        votes: votes,
+        id: anecdote.id,
+      }
+    })
+  }
+
+  const sortedAnecdotes = anecdotes.slice().sort((a, b) => b.votes - a.votes)
+
+  return (
+    <>
+      <h2>Anecdotes</h2>
+      {sortedAnecdotes.map(anecdote =>
+        <div key={anecdote.id}>
+          <div>
+            {anecdote.content}
+          </div>
+          <div>
+            has {anecdote.votes}
+            <button onClick={() => vote(anecdote)}>vote</button>
+          </div>
+        </div>
+      )}
+    </>
+  )
+}
+
+export default AnecdoteList
