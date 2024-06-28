51e0ccef2c06b958ccedae778d31d84a1456e0d6
Juho Kangas <juho.kangas@aalto.fi>
2024-06-28 19:58:03 +0300

    fix store and create notification slice 6.12

---
+++ Osa6/redux-anecdotes/src/reducers/notificationReducer.js
@@ -0,0 +1,8 @@
+import { createSlice } from '@reduxjs/toolkit';
+
+const notificationSlice = createSlice({
+  name: 'notification',
+  initialState: 'Initial state'
+});
+
+export default notificationSlice.reducer;
